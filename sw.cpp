c767ee9dc1a1ff6258d237786f3666933561dd9b
lzwdgc <lzwdgc@gmail.com>
2024-03-28 15:18:31 +0300

    Use versioned primitives.

--- sw.cpp
+++ sw.cpp
@@ -12,53 +12,54 @@ void build(Solution &s)
     auto &p = s.addProject("Polygon4.Bootstrap", "0.0.12");
     p += Git("https://github.com/aimrebirth/Bootstrap", "", "{v}");
 
+    auto cppstd = cpp23;
+
     auto &core = p.addTarget<StaticLibrary>("core");
 
     core.PackageDefinitions = true;
-    core += cpp20;
+    core += cppstd;
 
     core += "src/core/.*"_rr, "Bootstrap.json";
     core.Public += "src/core"_idir;
-    core.Public += "_WIN32_WINNT=0x0601"_def;
-    core.Public += "pub.egorpugin.primitives.http-master"_dep;
-    core.Public += "pub.egorpugin.primitives.log-master"_dep;
-    core.Public += "pub.egorpugin.primitives.pack-master"_dep;
-    core.Public += "pub.egorpugin.primitives.hash-master"_dep;
-    core.Public += "pub.egorpugin.primitives.command-master"_dep;
-    core.Public += "pub.egorpugin.primitives.executor-master"_dep;
-    core.Public += "pub.egorpugin.primitives.sw.main-master"_dep;
+    core.Public += "pub.egorpugin.primitives.http"_dep;
+    core.Public += "pub.egorpugin.primitives.log"_dep;
+    core.Public += "pub.egorpugin.primitives.pack"_dep;
+    core.Public += "pub.egorpugin.primitives.hash"_dep;
+    core.Public += "pub.egorpugin.primitives.command"_dep;
+    core.Public += "pub.egorpugin.primitives.executor"_dep;
+    core.Public += "pub.egorpugin.primitives.sw.main"_dep;
 
     {
         auto &t = p.addTarget<Executable>("developer");
-        t += cpp20;
+        t += cppstd;
         t += "src/bootstrap_developer.cpp";
         t += core;
     }
 
     {
         auto &t = p.addTarget<Executable>("release");
-        t += cpp20;
+        t += cppstd;
         t += "src/bootstrap_release.cpp";
         t += core;
     }
 
     {
         auto &t = p.addTarget<Executable>("tools");
-        t += cpp20;
+        t += cppstd;
         t += "src/bootstrap_tools.cpp";
         t += core;
     }
 
     {
         auto &t = p.addTarget<Executable>("updater", "0.0.1");
-        t += cpp20;
+        t += cppstd;
         t += "src/updater.cpp";
         t += core;
     }
 
     {
         auto &t = p.addTarget<Executable>("remove_untracked_content");
-        t += cpp20;
+        t += cppstd;
         t += "src/remove_untracked_content.cpp";
         t += core;
     }
